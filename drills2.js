6fca271be67dc23904a8c4bc82729150f838f65b
wjacobs71086 <w.jacobs.632@gmail.com>
2019-09-16 12:56:48 -0700

    Added to inifity and beyond

--- drills2.js
+++ drills2.js
@@ -4,3 +4,19 @@ function jediName(firstName, lastName){
 }
 
 console.log(jediName('Beyonce', 'Knowles'));
+
+function beyond(num){
+  if (num === Infinity){
+    console.log('And beyond');
+  } else {
+    if( num > 0 ) { 
+      console.log('To infinity');
+    }
+    if ( num < 0){
+      console.log('To negative infinity'); 
+    } 
+    if ( num === 0){
+      console.log('Staying home');
+    }
+  }
+}
