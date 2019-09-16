f5ccf397d9d32d554c4dc2feac664183ca54744e
Keisuke Isobe <keisuke.isobe.2011@gmail.com>
2019-09-16 16:22:19 -0400

    decoder

--- drills2.js
+++ drills2.js
@@ -3,8 +3,6 @@ function jediName(firstName, lastName){
   return `${lastName.substring(0,3) + firstName.substring(0,2)}`;
 }
 
-console.log(jediName('Beyonce', 'Knowles'));
-
 function beyond(num){
   if (num === Infinity){
     console.log('And beyond');
@@ -19,4 +17,23 @@ function beyond(num){
       console.log('Staying home');
     }
   }
+}
+
+function decode(sentence){
+  let decode = '';
+  let arraySentence = sentence.split(' ');
+  for (let i = 0; i<arraySentence.length; i++){
+    if (arraySentence[i][0] === 'a'){
+      decode = decode + arraySentence[i][1];
+    } else if (arraySentence[i][0] === 'b'){
+      decode = decode + arraySentence[i][2];
+    } else if (arraySentence[i][0] === 'c'){
+      decode = decode + arraySentence[i][3];
+    } else if (arraySentence[i][0] === 'd'){
+      decode = decode + arraySentence[i][4];
+    } else {
+      decode = decode + ' ';
+    }
+  }
+  return decode;
 }
